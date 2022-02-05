49dfeaa65c8be0ff3260df6d3e041a8cce897c40
Aashan Ghimire <ashanghimire10@gmail.com>
2022-02-06 01:15:33 +0545

    Introduced route function and added url that corresponds to the path regex in each route entry

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
+use Astro\Controller\Auth\RegisterController;
 use Astro\Controller\HomeController;
+use Astro\Exceptions\NotFoundException;
 
-return $routes = [
+global $routes;
+
+$routes = [
     'home' => [
         'path' => '/^\/$/',
+        'url' => '/',
         'action' => [
             'controller' => HomeController::class,
             'method' => 'index'
         ],
         'method' => 'GET',
     ],
+    'register' => [
+        'path' => '/^\/register$/',
+        'url' => '/register',
+        'action' => [
+            'controller' => RegisterController::class,
+            'method' => 'register'
+        ],
+        'method' => 'GET',
+    ],
     'about' => [
         'path' => '/^\/about$/',
+        'url' => '/about',
         'action' => [
             'controller' => HomeController::class,
             'method' => 'about'
@@ -21,6 +36,7 @@ return $routes = [
     ],
     'contact' => [
         'path' => '/^\/contact$/',
+        'url' => '/contact',
         'action' => [
             'controller' => HomeController::class,
             'method' => 'contact'
@@ -28,3 +44,17 @@ return $routes = [
         'method' => 'GET'
     ],
 ];
+
+
+function route(string $name): string
+{
+    global $routes;
+
+    if(isset($routes[$name])) {
+        $basePath = $_SERVER['HTTP_HOST'];
+        return $basePath . $routes[$name]['url'];
+    }
+    throw new NotFoundException( sprintf('Route `%s` is not registered. Please check your routes file!', $name));
+}
+
+return $routes;
